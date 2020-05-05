215092c8655e769f694b63deeab1972ff8ba9a06
Omar Dominguez <odomardominguezod@gmail.com>
2020-05-05 13:03:57 -0700

    Adding Menu and Volume Slider to Main Project

---
+++ IlluminatiProject323/Assets/Script/btnSound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btnSound : MonoBehaviour
+{
+   public AudioSource soundButton;
+   public AudioClip hover;
+   public AudioClip click;
+
+   public void HoverSound()
+   {
+      soundButton.PlayOneShot(hover);
+   }
+   
+   public void ClickSound()
+   {
+      soundButton.PlayOneShot(click);
+   }
+}
